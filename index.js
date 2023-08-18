73dab06e076a42f69d94f49e2eada0ad26f9557e
Asisipho <missfente@gmail.com>
2023-08-18 19:31:48 +0200

    added basic style

---
+++ index.js
@@ -0,0 +1,79 @@
+//import express  framework
+import express from 'express';
+//import Registration module
+import Registration from './registration.js';
+//import the handlebars engine 
+import exphbs from 'express-handlebars';
+//import body-parsers to handle the reading of template objects?
+import bodyParser from 'body-parser';
+
+//import express flash and session to use
+import flash from 'express-flash';
+import session from 'express-session';
+
+//import the database connection module
+import db from './db_connection.js';
+
+//instantiate express module
+let app = express();
+//configuring the handlebars module 
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+    secret: "<JesusLovesYou>",
+    resave: false,
+    saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
+
+// his ensures form variables can be read from the req.body variable
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//built-in static middleware from ExpressJS to use static resources such as my CSS
+app.use(express.static('public'))
+
+//instance of the factory function 
+let registration = Registration();
+
+//import Routes
+
+//Send objects by rending to the index using the Get Method
+app.get('/', (req, res) => {
+    let userReg = registration.getRegistrations();
+let selectedTown = registration.getSelectedTown();
+console.log(selectedTown);
+    res.render('index', {
+        car_registration: userReg,
+        selectTown: selectedTown
+    })
+})
+
+app.get('/reg_numbers', (req, res) => {
+ 
+})
+
+app.post('/reg_numbers', (req, res) => {
+    let car_reg = req.body.car_reg;
+    let town = req.body.towns;
+    registration.selectTown(town);
+    res.redirect('/');
+
+    registration.validRegistration(car_reg);
+    registration.addRegistrations(car_reg);
+    res.redirect('/');
+
+})
+
+//process the enviroment the port is running on
+let PORT = process.env.PORT || 3000;
+//listen on the port - opens the port on the terminal.
+app.listen(PORT, () => {
+    console.log('App started...', PORT);
+})
---
+++ registration.js
@@ -0,0 +1,84 @@
+export default function Registration() {
+    let registration_list =  []
+    let selectedItem = []
+
+    function validRegistration(user_registration) {
+        let regExpression = /^C[ALTJ][ ]\d{3}[- ]?\d{1,3}$/
+        return (regExpression.test(user_registration) === true) ? true : false
+
+    }
+
+    function addRegistrations(user_registration) {
+        if (registration_list[user_registration] == undefined) {
+            registration_list.push(user_registration)
+            registration_list[user_registration] = 0
+        }
+    }
+
+    function getRegistrations() {
+        return registration_list
+    }
+
+    function selectTown(dropdown_value) {
+        selectedItem = []
+        for (let i = 0; i < registration_list.length; i++) {
+            let firstTwoChars = registration_list[i].charAt(0) + registration_list[i].charAt(1)
+            if (dropdown_value == firstTwoChars || dropdown_value == 'all') {
+                selectedItem.push(registration_list[i])
+            }
+        } 
+    }
+
+    function getSelectedTown(){
+        return selectedItem
+    }
+
+    function isTownSelected() {
+        return (selectedItem.length === 0) ? 'No available registration' : ''
+    }
+
+    function clear() {
+        if (confirm('Are you sure you want to clear all registrations?') ) {
+            localStorage.clear();
+        }
+    }
+
+    function errors(reg) {
+        let errorMessage;
+        let sub = reg.substring(3);
+        let indicator = reg.charAt(1).toUpperCase()
+        if (reg == '' || reg == null) {
+            errorMessage = 'Please enter a vehicle registration'
+            return errorMessage
+        }
+        if (reg.charAt(0).toLowerCase() !== 'c') {
+            errorMessage = 'Registration must start with C'
+            return errorMessage
+        }
+        if (['A', 'L', 'T', 'J'].includes(indicator) == false) {
+            errorMessage = 'Registration must be from Cape Town(CA), Stellenbosch(CL), Ceres(CT) or Paarl(CJ)'
+            return errorMessage
+        }
+        if (sub.length < 4) {
+            errorMessage = 'Registration is too short.'
+            return errorMessage
+        }
+        if (sub.length > 7) {
+            errorMessage = 'Registration is too long.'
+            return errorMessage
+        }
+        return ''
+    }
+
+    return {
+        addRegistrations,
+        getRegistrations,
+        getSelectedTown,
+        selectTown,
+        isTownSelected,
+        validRegistration,
+        clear,
+        errors
+    }
+
+}
